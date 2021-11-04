3acf5681f6037139491b6b3ed710ed2c24282658
André <AndreR@outlook.de>
2021-11-04 16:47:00 +0100

    FUn mit .NET 6

---
+++ HalloNET/consolenFun/AAAAAAA/BBBBB/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace consolenFun.AAAAAAA.BBBBB
+{
+    internal class Class1
+    {
+        int _zahl;
+
+        public string? Name { get; set; }
+
+        public Class1? MyProperty { get; set; }
+
+        public int Zahl { get => _zahl; set => _zahl = value; }
+
+
+
+        public IQueryable<Class1> Dings(int zahl)
+        {
+            _zahl += zahl;
+            Console.WriteLine(Name);
+            return new List<Class1>().AsQueryable<Class1>();
+        }
+
+    }
+}
---
+++ HalloNET/consolenFun/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("Hello, WorldAAAAAAAA!");
+fewklhfbwekjnfikjnneMethode("AAAAA");
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine(i);
+}
+
+static  void fewklhfbwekjnfikjnneMethode(string KJHBKJN)
+{
+    Console.WriteLine(KJHBKJN);
+}
