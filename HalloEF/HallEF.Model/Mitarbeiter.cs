ca33496d7d217156f39943170ce37aed4daac870
André <AndreR@outlook.de>
2021-11-05 15:31:49 +0100

    Entity Framework 6 Code First mit Bogus

---
+++ HalloEF/HallEF.Model/Mitarbeiter.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace HalloEF.Model
+{
+    public class Mitarbeiter : Person
+    {
+        public string Beruf { get; set; }
+        public virtual ICollection<Kunde> Kunden { get; set; } = new HashSet<Kunde>();
+        public virtual ICollection<Abteilung> Abteilungen { get; set; } = new HashSet<Abteilung>();
+    }
+
+}
---
+++ HalloEF/HalloEF/EfContext.cs
@@ -0,0 +1,35 @@
+﻿using HalloEF.Model;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace HalloEF
+{
+    internal class EfContext : DbContext
+    {
+        public DbSet<Person> Personen { get; set; }
+        public DbSet<Mitarbeiter> Mitarbeiter { get; set; }
+        public DbSet<Kunde> Kunden { get; set; }
+        public DbSet<Abteilung> Abteilungen { get; set; }
+
+        public EfContext() : base("Server=(localdb)\\mssqllocaldb;Database=HalloEF;Trusted_Connection=true")
+        {
+
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Mitarbeiter>().ToTable("Mitarbeiter");
+            modelBuilder.Entity<Kunde>().ToTable("Kunden");
+            modelBuilder.Entity<Person>().ToTable("Personen");
+
+            modelBuilder.Entity<Mitarbeiter>().Property(x => x.Beruf)
+                                              .IsRequired()
+                                              .HasColumnName("Job")
+                                              .HasMaxLength(68);
+
+
+        }
+    }
+}
